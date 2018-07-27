74273e9a3a3f084e80e7eca7e2c742095f502b5f
Abhishek Girish <agirish@apache.org>
2018-07-27 14:42:39 -0700

    Removing package name

--- src/main/java/ComplexJSON.java
+++ src/main/java/ComplexJSON.java
@@ -1,5 +1,3 @@
-package tpch_complex;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/csv.java
+++ src/main/java/csv.java
@@ -1,4 +1,3 @@
-package tpch_complex;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
 import java.io.InputStreamReader;
